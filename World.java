3b3e35e5fa5e771a542f9d76e2600293a97981c6
bpap <bogumila.papiernik@gmail.com>
2021-10-14 23:46:55 +0200

    v.0.1

--- World.java
+++ World.java
@@ -5,7 +5,22 @@ public class World {
         System.out.println("Start");
 
         Direction[] dirs = new Direction[args.length];
-        transform(args, dirs);
+        for (int i=0; i<args.length; i++){
+            switch (args[i]){
+                case "l":
+                    dirs[i] = Direction.l;
+                    break;
+                case "r":
+                    dirs[i] = Direction.r;
+                    break;
+                case "f":
+                    dirs[i] = Direction.f;
+                    break;
+                case "b":
+                    dirs[i] = Direction.b;
+                    break;
+            }
+        }
 
         run(dirs);
 
