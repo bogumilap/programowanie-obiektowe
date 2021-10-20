4831697323d046f586a9cb7b9107a7c366312921
bpap <bogumila.papiernik@gmail.com>
2021-10-20 19:01:19 +0200

    v.1.0

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -2,12 +2,15 @@ package agh.ics.oop;
 
 public class World {
     public static void main(String[] args){
-        System.out.println("Start");
+//        System.out.println("Start");
+//        run(transform(args));
+//        System.out.println("Stop");
 
-        run(transform(args));
-
-        System.out.println("Stop");
-
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
     }
 
 
