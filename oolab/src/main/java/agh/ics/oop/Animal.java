a8b150650b8d1f28a8e309177c40c4dc05edae80
bpap <bogumila.papiernik@gmail.com>
2021-11-12 18:03:10 +0100

    changed move() method

--- oolab/src/main/java/agh/ics/oop/Animal.java
+++ oolab/src/main/java/agh/ics/oop/Animal.java
@@ -2,30 +2,47 @@ package agh.ics.oop;
 
 public class Animal {
     private MapDirection orientaion = MapDirection.NORTH;
-    private Vector2d location = new Vector2d(2, 2);
+    private Vector2d location; // = new Vector2d(2, 2);
+    private IWorldMap map;
+
+    public Animal() {
+        location = new Vector2d(2, 2);
+        map = new RectangularMap(5, 5);
+    }
+
+    public Animal(IWorldMap map) {
+        this.map = map;
+    }
+
+    public Animal(IWorldMap map, Vector2d initialPosition) {
+        this.map = map;
+        this.location = initialPosition;
+    }
 
     @Override
     public String toString(){
-        String str = orientaion + ", (" + location.getX() + ", " + location.getY() + ")";
-        return str;
+        return switch (orientaion) {
+            case NORTH -> "N";
+            case SOUTH -> "S";
+            case EAST -> "E";
+            case WEST -> "W";
+        };
     }
 
     public void move(MoveDirection direction) {
         switch (direction) {
             case RIGHT -> orientaion = orientaion.next();
             case LEFT -> orientaion  = orientaion.previous();
-            case FORWARD -> {
-                Vector2d new_location = location.add(orientaion.toUnitVector());
-                if (new_location.getX() < 5 && new_location.getX() >= 0
-                        && new_location.getY() < 5 && new_location.getY() >= 0) {
-                    location = new_location;
+            case FORWARD, BACKWARD -> {
+                Vector2d step = orientaion.toUnitVector();
+                if (direction == MoveDirection.BACKWARD) {
+                    step = step.opposite();
                 }
-            }
-            case BACKWARD -> {
-                Vector2d new_polozenie = location.subtract(orientaion.toUnitVector());
-                if (new_polozenie.getX() >= 0 && new_polozenie.getX() < 5
-                        && new_polozenie.getY() >= 0 && new_polozenie.getY() < 5) {
-                    location = new_polozenie;
+
+                Vector2d new_location = location.add(step);
+                if (map.canMoveTo(new_location)) {
+                    location.setX(new_location.getX());
+                    location.setY(new_location.getY());
                 }
             }
         }
