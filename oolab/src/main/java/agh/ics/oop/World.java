3fcfb0eaf06edde5f32fe221306e1e6f5052a8e9
bpap <bogumila.papiernik@gmail.com>
2021-11-21 18:31:51 +0100

    main

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -3,7 +3,7 @@ package agh.ics.oop;
 public class World {
     public static void main(String[] args) {
         MoveDirection[] directions = new OptionsParser().parse(args);
-        IWorldMap map = new RectangularMap(10, 5);
+        IWorldMap map = new GrassField(10);
         Vector2d[] positions = {new Vector2d(2,2), new Vector2d(3,4)};
         IEngine engine = new SimulationEngine(directions, map, positions);
         engine.run();
