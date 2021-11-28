afd7dfc5a73f3c165b188835117649f9651d88f8
bpap <bogumila.papiernik@gmail.com>
2021-11-28 15:58:41 +0100

    added list of animals

--- oolab/src/main/java/agh/ics/oop/SimulationEngine.java
+++ oolab/src/main/java/agh/ics/oop/SimulationEngine.java
@@ -1,11 +1,13 @@
 package agh.ics.oop;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class SimulationEngine implements IEngine {
     private final MoveDirection[] directions;
     private IWorldMap map;
     private final Vector2d[] positions;
+    private List<Animal> animals = new LinkedList<>();
 
     public SimulationEngine(MoveDirection[] directions, IWorldMap map, Vector2d[] positions) {
         this.directions = directions;
@@ -18,13 +20,13 @@ public class SimulationEngine implements IEngine {
         for (Vector2d position : positions) {
             Animal animal = new Animal(map, position);
             map.place(animal);
+            animals.add(animal);
         }
     }
 
     @Override
     public void run() {
         int count = 0;
-        List<Animal> animals = map.animals;
         int numOfAnimals = animals.size();
         while (count < directions.length) {
             Animal currentAnimal = animals.get(count % numOfAnimals);
